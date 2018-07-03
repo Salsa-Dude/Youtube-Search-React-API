71116e3aea22d6f0738f16da39a344c85901c97d
Joseph Arias <arias.joseph.a@gmail.com>
2018-07-02 22:46:56 -0400

    components

--- src/index.js
+++ src/index.js
@@ -1,19 +1,43 @@
 
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
-
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyDq7pXNU5Cqoo2JWx_MnWc4fmFrnu4radw';
+
+
 
 // Create a new component. This component should produce some HTMl
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    YTSearch({key: API_KEY, term: 'soccer'}, (data) => {
+      this.setState({
+        videos: data,
+        selectedVideo: data[0]
+      });
+    });
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo: selectedVideo})}
+          videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
